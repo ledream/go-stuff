8a9f35c801f784b407a1f43b5dad6fda77511c08
dramanebamba <dramane.bamba@gmail.com>
2017-02-10 01:21:21 +0100

    flagLib

--- flag.go
+++ flag.go
@@ -1,10 +1,14 @@
 package main
 import "flag"
 
-var helpMessage = "-help -- for help"
+var b = flag.String("b", "", "number non-blank output lines")
+var e = flag.String("e", "", "display $ at the end of each line (implies -v)")
+var n = flag.String("n", "", "number all output lines")
+var s = flag.String("s", "", "squeeze multiple blank lines into one")
+var t = flag.String("t", "", "display tab as ^I (implies -v)")
+var u = flag.String("u", "", "do not buffer output")
+var v = flag.String("v", "", "display non-printing chars as ^X or M-a")
 
-var help = flag.String("help", "", helpMessage)
-var file = flag.String("file", "", helpMessage)
 
 func main()  {
 	flag.Parse();
